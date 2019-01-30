71b0444ffd895ef080898a0ddb2662c5061d8590
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2019-01-30 15:22:02 -0500

    finish resolvers test-3

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -10,9 +10,38 @@ const productsTypeMatcher = {
 }
 
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    async product(parent, {id}, ctx, info){
+      const product = await Product.findById(id);
+      return product;
+    },
+    async products(parent, args, ctx, info){
+      return Product.find({}).exec();
+    }
+  },
+  Mutation: {
+    async newProduct(parent, args, ctx, info){
+      const product = await Product.create({...args.input, createdBy: ctx.user});
+      return product;
+    },
+    updateProduct(parent, {id, input}, ctx, info){
+      // TODO: make sure person owns product or is an admin!
+      return Product.findByIdAndUpdate(id, input, {
+        new: true,
+        runValidators: true,
+      })
+    },
+    async removeProduct(parent, args, ctx, info){
+      return Product.findByIdAndRemove({_id: args.id});
+    }
+  },
   Product: {
-    __resolveType(product) {}
-  }
+    // entire product comes in for parent arg
+    __resolveType(product) {},
+    async createdBy({createdBy}){
+      console.log('in created by resolver')
+      return User.findById(createdBy).exec();
+    }
+  },
+
 }
